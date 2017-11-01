2e67fda47c456a9cf87d0bf53770bb88dc9e0f0b
Morgan Davis <mkd@goosebox.local>
2017-10-31 19:45:55 -0700

    Some notes on geometry primitives

---
+++ tests/scenekit/04-primitivegeometries/04-primitivegeometries/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  04-primitivegeometries
+//
+//  Created by Morgan Davis on 10/31/17.
+//  Copyright © 2017 Morgan Davis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
