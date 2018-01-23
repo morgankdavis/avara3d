4e5f7fbde4c670fedd43b73cf00728a9b090d398
Morgan Davis <m@goosesensor.net>
2018-01-22 19:09:31 -0800

    Fixed trackpad input in MINGW

--- avara-engine/external/manymouse/windows_wminput.c
+++ avara-engine/external/manymouse/windows_wminput.c
@@ -20,7 +20,6 @@
 #include <windows.h>
 #include <setupapi.h>
 #include <malloc.h>  /* needed for alloca(). */
-#include <stdio.h> // TEMPORARY MORGAN
 
 /* Cygwin's headers don't have WM_INPUT right now... */
 #ifndef WM_INPUT
@@ -217,15 +216,12 @@ static void queue_event(const ManyMouseEvent *event)
 
 static void queue_from_rawinput(const RAWINPUT *raw)
 {
-    printf("queue_from_rawinput()\n");
-
     int i;
     const RAWINPUTHEADER *header = &raw->header;
     const RAWMOUSE *mouse = &raw->data.mouse;
     ManyMouseEvent event;
 
     if (raw->header.dwType != RIM_TYPEMOUSE) {
-        printf("raw->header.dwType != RIM_TYPEMOUSE\n");
         return;
     }
 
@@ -235,10 +231,13 @@ static void queue_from_rawinput(const RAWINPUT *raw)
             break;
     } /* for */
 
+    // modified by Morgan
+    // from some reason MinGW/64 is reporting header->hDevice (above) as 0 ??
+#ifndef MINGW
     if (i == available_mice) {
-        printf("i == available_mice\n");
         return;  /* not found?! */
     }
+#endif
 
     /*
      * RAWINPUT packs a bunch of events into one, so we split it up into
@@ -588,12 +587,10 @@ static int windows_wminput_init(void)
 
     if (!init_event_queue())
     {
-        printf("!init_event_queue()\n");
         cleanup_window();
         available_mice = 0;
     } /* if */
 
-    printf("available_mice: %d\n", available_mice);
     return available_mice;
 } /* windows_wminput_init */
 
