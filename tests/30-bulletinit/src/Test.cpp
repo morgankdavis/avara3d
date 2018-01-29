a5cc60afb9e013fd977aa3f1d3c01b79dab67db6
Morgan Davis <m@goosesensor.net>
2018-01-28 17:38:55 -0800

    Sync for laptop.

---
+++ tests/30-bulletinit/src/Test.cpp
@@ -0,0 +1,268 @@
+//
+//  Test.cpp
+//	avara-engine
+//
+//  Created by Morgan Davis on 1/27/18.
+//  Copyright © 2018 Morgan K Davis. All rights reserved.
+//
+
+#include "Test.h"
+
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+#include "ae.h"
+#include "Utilities.h"
+
+
+using namespace ae;
+using namespace ae::utils;
+using namespace std;
+using namespace std::placeholders;
+using namespace glm;
+
+
+#define ENABLE_HIGH_DPI        	true
+#define WINDOW_WIDTH			800
+#define WINDOW_HEIGHT			600
+#define FULLSCREEN 				false
+#define ANTIALIASING_MODE		AntialiasingMode_4X
+#define ENABLE_VSYNC			false
+#define CAPTURE_CURSOR			true
+#define MOUSE_SENSITIVITY		0.5f
+
+
+/***************************************************************************************
+     MARK:   Public
+ **************************************************************************************/
+
+int Test::run(const vector<string>& args) {
+	LoggerSink sinks = (LoggerSink)0;
+	sinks = (LoggerSink)(sinks | (LoggerSink)LoggerSink_STDOUT);
+	//sinks = (LoggerSink)(sinks | (LoggerSink)LoggerSink_MainFile);
+	//sinks = (LoggerSink)(sinks | (LoggerSink)LoggerSink_NamedFile);
+	auto logger = make_shared<Logger>("test30", sinks);
+	
+	auto window = Window(FULLSCREEN, WINDOW_WIDTH, WINDOW_HEIGHT, ENABLE_HIGH_DPI, ANTIALIASING_MODE);
+	logger->info("Test::run()");
+	
+	window.updateCallback(bind(&Test::windowUpdateCallback, this, _1, _2));
+	window.willRenderCallback(bind(&Test::windowWillRenderCallback, this, _1, _2));
+	window.didRenderCallback(bind(&Test::windowDidRenderCallback, this, _1, _2));
+	window.captureCursor(CAPTURE_CURSOR);
+	window.enableVSync(ENABLE_VSYNC);
+	window.debugOptions(DebugOption_ShowStatsOveray);
+	m_window = &window;
+
+	
+	auto scene = make_shared<Scene>();
+	
+	
+	const float PLANE_DIM = 25.0;
+	auto planeNode = make_shared<Node>(make_shared<Plane>(PLANE_DIM, PLANE_DIM));
+	auto planeMaterialProperty = make_shared<MaterialProperty>(make_shared<Color>(0.75, 0.75, 0.75, 1.0));
+	auto planeMaterial = make_shared<Material>(nullptr, planeMaterialProperty, nullptr);
+	planeMaterial->doubleSided(true);
+	planeNode->geometry()->addMaterial(planeMaterial);
+	planeNode->rotation({1, 0, 0, radians(90.0)});
+	planeNode->position({planeNode->position().x,
+		planeNode->position().y - PLANE_DIM,
+		planeNode->position().z});
+	scene->rootNode()->addChildNode(planeNode);
+	
+	
+	
+	
+	
+	const float BOX_DIM = 1.0;
+	auto boxNode = make_shared<Node>(make_shared<Box>(BOX_DIM, BOX_DIM, BOX_DIM));
+	auto boxMaterialProperty = make_shared<MaterialProperty>(make_shared<Color>(1.0, 0, 0, 1.0));
+	auto boxMaterial = make_shared<Material>(nullptr, boxMaterialProperty, nullptr);
+	boxNode->geometry()->addMaterial(boxMaterial);
+	boxNode->position({0, 10, 0});
+	scene->rootNode()->addChildNode(boxNode);
+	
+	
+	
+	
+	
+	
+	auto physicsWorld = make_shared<PhysicsWorld>();
+	scene->physicsWorld(physicsWorld);
+	
+	
+	
+	
+
+	auto background = make_shared<MaterialProperty>(TestCubeNamed("sky1", "png"));
+	scene->background(background);
+
+	auto ambientLight = make_shared<Light>(LightType_Ambient, make_shared<Color>(0.75, 0.75, 0.75, 1.0));
+	auto ambientLightNode = make_shared<Node>(ambientLight);
+	scene->rootNode()->addChildNode(ambientLightNode);
+
+	auto pointLight = make_shared<Light>(LightType_Point, make_shared<Color>(Color::White()));
+	pointLight->attenuationFactor(0.000000015);
+	auto pointLightNode = make_shared<Node>(pointLight);
+	scene->rootNode()->addChildNode(pointLightNode);
+
+	pointLightNode->position({25, 25, 25});
+
+//	auto materialProperty = make_shared<MaterialProperty>(pointLight->color());
+//	auto material = make_shared<Material>();
+//	material->name("LIGHT material");
+//	material->emissive(materialProperty);
+//	auto geometry = make_shared<Sphere>(3.5, 16);
+//	geometry->addMaterial(material);
+//	pointLightNode->geometry(geometry);
+
+
+	scene->fogStartDistance(500.0);
+	scene->fogEndDistance(5000.0);
+	scene->fogDensityExponent(1.0);
+	scene->fogColor(make_shared<Color>(Color::LightGray()));
+
+	
+	window.scene(scene);
+	m_inputManager = window.inputManager();
+	window.display();
+	
+	return 0;
+}
+
+/***************************************************************************************
+     MARK:   Window Callbacks
+ **************************************************************************************/
+
+void Test::windowUpdateCallback(Scene& scene, float time) {
+	
+	static double previousSeconds = time;
+	float deltaSeconds = time - previousSeconds;
+	previousSeconds = time;
+	
+	// get input
+	
+	auto keysPressed = m_inputManager->keysPressed();
+	
+	if (keysPressed.count(Key_Escape)) {
+		exit(0);
+	}
+	
+	DebugOption options = (DebugOption)m_window->debugOptions();
+	if (keysPressed.count(Key_F)) {
+		if (m_window->debugOptions() & DebugOption_ShowWireframe) {
+			m_window->debugOptions((DebugOption)(options & ~DebugOption_ShowWireframe));
+		}
+		else {
+			m_window->debugOptions((DebugOption)(options | DebugOption_ShowWireframe));
+		}
+	}
+	if (keysPressed.count(Key_B)) {
+		if (m_window->debugOptions() & DebugOption_ShowBoundingBoxes) {
+			m_window->debugOptions((DebugOption)(options & ~DebugOption_ShowBoundingBoxes));
+		}
+		else {
+			m_window->debugOptions((DebugOption)(options | DebugOption_ShowBoundingBoxes));
+		}
+	}
+	if (keysPressed.count(Key_I)) {
+		if (m_window->debugOptions() & DebugOption_ShowStatsOveray) {
+			m_window->debugOptions((DebugOption)(options & ~DebugOption_ShowStatsOveray));
+		}
+		else {
+			m_window->debugOptions((DebugOption)(options | DebugOption_ShowStatsOveray));
+		}
+	}
+
+	if (keysPressed.count(Key_V)) {
+		m_window->enableVSync(!(m_window->vSyncEnabled()));
+	}
+	
+	if (keysPressed.count(Key_Backslash)) {
+		SaveSnapshot(*m_window);
+	}
+	
+	if (keysPressed.count(Key_R)) {
+		if (!m_window->recordingGIF()) {
+			StartGIFRecording(*m_window, 240, 8);
+		}
+		else {
+			StopGIFRecording(*m_window);
+		}
+	}
+	
+	// mouselook
+	
+	vec2 mousePositionDelta = m_inputManager->mousePositionDelta();
+	
+	static const float mouseSensitivity = (1.0f / MOUSE_SENSITIVITY);
+	
+	if (!m_cameraNode) {
+		for (auto n : scene.rootNode()->childNodes(false)) {
+			if (n->camera()) {
+				m_cameraNode = n;
+				break;
+			}
+		}
+	}
+	
+	if (m_cameraNode) {
+		
+		// look
+		
+		vec3 camForward = m_cameraNode->worldForward();
+		vec3 camRight = m_cameraNode->worldRight();
+		vec3 camUp = m_cameraNode->worldUp();
+		
+		float deltaRotX = atan(deltaSeconds * mousePositionDelta.x / mouseSensitivity);
+		float deltaRotY = atan(deltaSeconds * mousePositionDelta.y / mouseSensitivity);
+		
+//		float deltaRotX = deltaSeconds * mousePositionDelta.x / mouseSensitivity;
+//		float deltaRotY = deltaSeconds * mousePositionDelta.y / mouseSensitivity;
+		
+		vec3 angles = m_cameraNode->eulerAngles();
+		m_cameraNode->eulerAngles(vec3(angles.x + deltaRotY, angles.y - deltaRotX, 0));
+		
+		// move
+		
+		//		const static float MOVE_SPEED = 5.0f; // units/sec
+		static float MOVE_SPEED = 0;
+		if (!MOVE_SPEED) MOVE_SPEED = Max(scene.extent());
+		
+		auto keysDown = m_inputManager->keysDown();
+		
+		if(keysDown.count(Key_W)) {
+			vec3 positionDelta = deltaSeconds * MOVE_SPEED * camForward;
+			m_cameraNode->position(m_cameraNode->position() + positionDelta);
+		}
+		else if(keysDown.count(Key_S)) {
+			vec3 positionDelta = deltaSeconds * MOVE_SPEED * -camForward;
+			m_cameraNode->position(m_cameraNode->position() + positionDelta);
+		}
+		
+		if(keysDown.count(Key_A)) {
+			vec3 positionDelta = deltaSeconds * MOVE_SPEED * -camRight;
+			m_cameraNode->position(m_cameraNode->position() + positionDelta);
+		}
+		else if(keysDown.count(Key_D)) {
+			vec3 positionDelta = deltaSeconds * MOVE_SPEED * camRight;
+			m_cameraNode->position(m_cameraNode->position() + positionDelta);
+		}
+		
+		if(keysDown.count(Key_Space)) {
+			vec3 positionDelta = deltaSeconds * MOVE_SPEED * camUp;
+			m_cameraNode->position(m_cameraNode->position() + positionDelta);
+		}
+	}
+}
+
+void Test::windowWillRenderCallback(Scene& scene, float time) {
+	
+}
+
+void Test::windowDidRenderCallback(Scene& scene, float time) {
+	
+}
+
+
---
+++ tests/30-bulletinit/src/Test.h
@@ -0,0 +1,48 @@
+//
+//  Test.h
+//	avara-engine
+//
+//  Created by Morgan Davis on 1/27/18.
+//  Copyright © 2018 Morgan K Davis. All rights reserved.
+//
+
+#ifndef Test_h
+#define Test_h
+
+
+#include <memory>
+#include <string>
+#include <vector>
+
+//#include "Types.h"
+
+
+namespace ae {
+
+
+	class InputManager;
+	class Node;
+	class Scene;
+	class Window;
+
+	
+	class Test {
+		
+	public:
+		
+		int run(const std::vector<std::string>& args);
+		
+		void windowUpdateCallback(Scene& scene, float time);
+		void windowWillRenderCallback(Scene& scene, float time);
+		void windowDidRenderCallback(Scene& scene, float time);
+		
+	private:
+
+		Window*								m_window;
+		std::shared_ptr<InputManager>		m_inputManager;
+		std::shared_ptr<Node>				m_cameraNode;
+	};
+}
+
+
+#endif /* Test_h */
