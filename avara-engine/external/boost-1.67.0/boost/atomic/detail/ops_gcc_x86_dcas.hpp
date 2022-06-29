987db368f7a4e2c195ff6eb587db85de93a91b37
Morgan Davis <mkd@debian-BULLSEYE-live-builder-AMD64>
2022-06-29 02:33:06 -0700

    Fixed macOS cross-compile error due to Bullet -Wargument-outside-range

--- avara-engine/external/boost-1.67.0/boost/atomic/detail/ops_gcc_x86_dcas.hpp
+++ avara-engine/external/boost-1.67.0/boost/atomic/detail/ops_gcc_x86_dcas.hpp
@@ -399,7 +399,8 @@ struct gcc_dcas_x86_64
         );
     }
 
-    static BOOST_FORCEINLINE storage_type load(storage_type const volatile& storage, memory_order) BOOST_NOEXCEPT
+	// MKD: storage used to be const
+    static BOOST_FORCEINLINE storage_type load(storage_type volatile& storage, memory_order) BOOST_NOEXCEPT
     {
 #if defined(__clang__)
 
