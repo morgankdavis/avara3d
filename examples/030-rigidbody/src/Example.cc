ab0aeceef9ba2aae994e4ab24870a507570289b0
Morgan Davis <mkd@mkd.net>
2023-11-17 19:34:47 -0800

    Setup for debugging bt compound bodies.

--- examples/030-rigidbody/src/Example.cc
+++ examples/030-rigidbody/src/Example.cc
@@ -42,7 +42,7 @@ constexpr ANTIALIASING_MODE		MSAA_MODE =				ANTIALIASING_MODE::MSAA_4X;
 constexpr bool					ENABLE_VSYNC =			false;
 constexpr bool					CAPTURE_CURSOR =		false;
 constexpr float					MOUSE_SENSITIVITY =		0.5;
-constexpr float					PHYSICS_TIMESTEP =		1.0/60.0;
+constexpr float					PHYSICS_TIMESTEP =		1.0/120.0;
 
 
 /***************************************************************************************
@@ -186,83 +186,83 @@ int Example::run(const vector<string>& args) {
 
 
 
-
-	// add the palm tree
-
-	auto palmScene = SceneNamed("palm2/palm2", "obj");
-	_palmNode = palmScene->rootNode();
-	_palmNode->name("Palm node");
-	for (auto n : palmScene->rootNode()->children(true)) {
-		if (n->geometry()) {
-			for (auto m : n->geometry()->materials()) {
-				m->doubleSided(true);
-			}
-		}
-	}
-
-	auto palmPhysicsBody = PhysicsBody::StaticBody();
-	palmPhysicsBody->mass(0);
-	palmPhysicsBody->friction(1);
-	palmPhysicsBody->restitution(0.25);
-	_palmNode->physicsBody(palmPhysicsBody);
-
-	scene->rootNode()->addChild(palmScene->rootNode());
-
-
-	// add the duck
-
-	_duckNode = SceneNamed("rubberDuck/rubberDuck", "obj")->rootNode()->child("g duck", false);
-	AE_LOG_I("DUCK NODE: {}", StringFromTree(*_duckNode));
-	_duckNode->position({4.5, 15, 0});
-
-
-	// #0
-	_duckNode->physicsBody(PhysicsBody::KinematicBody());
-	_duckNode->physicsBody()->shape()->type(PHYSICS_SHAPE_TYPE::CONCAVE_POLYHEDRON);
-
-	// #1
+//
+//	// add the palm tree
+//
+//	auto palmScene = SceneNamed("palm2/palm2", "obj");
+//	_palmNode = palmScene->rootNode();
+//	_palmNode->name("Palm node");
+//	for (auto n : palmScene->rootNode()->children(true)) {
+//		if (n->geometry()) {
+//			for (auto m : n->geometry()->materials()) {
+//				m->doubleSided(true);
+//			}
+//		}
+//	}
+//
+//	auto palmPhysicsBody = PhysicsBody::StaticBody();
+//	palmPhysicsBody->mass(0);
+//	palmPhysicsBody->friction(1);
+//	palmPhysicsBody->restitution(0.25);
+//	_palmNode->physicsBody(palmPhysicsBody);
+//
+//	scene->rootNode()->addChild(palmScene->rootNode());
+//
+//
+//	// add the duck
+//
+//	_duckNode = SceneNamed("rubberDuck/rubberDuck", "obj")->rootNode()->child("g duck", false);
+//	AE_LOG_I("DUCK NODE: {}", StringFromTree(*_duckNode));
+//	_duckNode->position({4.5, 15, 0});
+//
+//
+//	// #0
 //	_duckNode->physicsBody(PhysicsBody::KinematicBody());
-//	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONVEX_HULL, _duckNode);
-//	_duckNode->physicsBody()->shape(duckPhysicsShape);
-
-	// #2
-//	_duckNode->physicsBody(PhysicsBody::KinematicBody());
-//	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONVEX_HULL, _duckNode->geometry());
-//	_duckNode->physicsBody()->shape(duckPhysicsShape);
-
-	// #3
-//	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONCAVE_POLYHEDRON, _duckNode);
-//	_duckNode->physicsBody(make_shared<PhysicsBody>(PHYSICS_BODY_TYPE::KINEMATIC, duckPhysicsShape));
-
-	// #4
-//	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONCAVE_POLYHEDRON, _duckNode->geometry());
-//	_duckNode->physicsBody(make_shared<PhysicsBody>(PHYSICS_BODY_TYPE::KINEMATIC, duckPhysicsShape));
-
-	_duckSpinnerNode = make_shared<Node>("duck spinner");
-	_duckSpinnerNode->addChild(_duckNode);
-	scene->rootNode()->addChild(_duckSpinnerNode);
-
-	
-
-	// add the paddle
-
-	_paddleNode = Node::GeometryNode(make_shared<Box>(.5, 5, 20));
-	auto paddleProperty = make_shared<MaterialProperty>(Color::Red());
-	auto paddleMaterial = make_shared<Material>(nullptr, paddleProperty, nullptr);
-	_paddleNode->geometry()->addMaterial(paddleMaterial);
-	_paddleNode->position({15 - .25, 2.5, 0});
-	auto paddlePhysicsBody = PhysicsBody::KinematicBody();
-//	paddlePhysicsBody->friction(100);
-	paddlePhysicsBody->restitution(0.25);
-	_paddleNode->physicsBody(paddlePhysicsBody);
-	scene->rootNode()->addChild(_paddleNode);
-
-
-	// add cardboard boxes
-	AddCardboardBoxes(*scene);
-
-	// add slurms
-	AddSlurms(*scene);
+//	_duckNode->physicsBody()->shape()->type(PHYSICS_SHAPE_TYPE::CONCAVE_POLYHEDRON);
+//
+//	// #1
+////	_duckNode->physicsBody(PhysicsBody::KinematicBody());
+////	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONVEX_HULL, _duckNode);
+////	_duckNode->physicsBody()->shape(duckPhysicsShape);
+//
+//	// #2
+////	_duckNode->physicsBody(PhysicsBody::KinematicBody());
+////	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONVEX_HULL, _duckNode->geometry());
+////	_duckNode->physicsBody()->shape(duckPhysicsShape);
+//
+//	// #3
+////	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONCAVE_POLYHEDRON, _duckNode);
+////	_duckNode->physicsBody(make_shared<PhysicsBody>(PHYSICS_BODY_TYPE::KINEMATIC, duckPhysicsShape));
+//
+//	// #4
+////	auto duckPhysicsShape = make_shared<PhysicsShape>(PHYSICS_SHAPE_TYPE::CONCAVE_POLYHEDRON, _duckNode->geometry());
+////	_duckNode->physicsBody(make_shared<PhysicsBody>(PHYSICS_BODY_TYPE::KINEMATIC, duckPhysicsShape));
+//
+//	_duckSpinnerNode = make_shared<Node>("duck spinner");
+//	_duckSpinnerNode->addChild(_duckNode);
+//	scene->rootNode()->addChild(_duckSpinnerNode);
+//
+//
+//
+//	// add the paddle
+//
+//	_paddleNode = Node::GeometryNode(make_shared<Box>(.5, 5, 20));
+//	auto paddleProperty = make_shared<MaterialProperty>(Color::Red());
+//	auto paddleMaterial = make_shared<Material>(nullptr, paddleProperty, nullptr);
+//	_paddleNode->geometry()->addMaterial(paddleMaterial);
+//	_paddleNode->position({15 - .25, 2.5, 0});
+//	auto paddlePhysicsBody = PhysicsBody::KinematicBody();
+////	paddlePhysicsBody->friction(100);
+//	paddlePhysicsBody->restitution(0.25);
+//	_paddleNode->physicsBody(paddlePhysicsBody);
+//	scene->rootNode()->addChild(_paddleNode);
+//
+//
+//	// add cardboard boxes
+//	AddCardboardBoxes(*scene);
+//
+//	// add slurms
+//	AddSlurms(*scene);
 
 
 
@@ -648,6 +648,10 @@ shared_ptr<Node> SpawnDuckFruit(Scene& scene, shared_ptr<Node> duckNode) {
 	float time = scene.renderContext().lock()->sceneTime();
 	static float lastSSpawnTime = 0;
 	if ((time - lastSSpawnTime) >= (1.0/SPAWN_RATE)) {
+
+		AddCardboardBox(scene, {0, 10, 0}, {1, 0, 0}, 0.0f);
+		lastSSpawnTime = time;
+		return nullptr;
 		
 		
 		unsigned fruitNum = Uniform(0, 5);
@@ -908,7 +912,7 @@ shared_ptr<Node> ShootBall(Scene& scene, const vec3& location, const vec3& direc
 
 
 
-		constexpr float BALL_VELOCITY = 45.0;
+		constexpr float BALL_VELOCITY = 50.0;
 		constexpr float DIRECTION_VARIATION = 0.025f;
 		auto variedDirection = direction + vec3(Uniform(-DIRECTION_VARIATION, DIRECTION_VARIATION),
 												Uniform(-DIRECTION_VARIATION, DIRECTION_VARIATION),
