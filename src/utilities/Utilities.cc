d594102d5133852e7df7c95e24d2fcc18ac69456
morgankdavis <7cGE5sQv>
2023-12-10 23:32:13 -0800

    Fix Windows?

--- src/utilities/Utilities.cc
+++ src/utilities/Utilities.cc
@@ -9,6 +9,11 @@
 #include "utilities/Utilities.h"
 
 #include <algorithm>
+#ifdef WINDOWS
+	// stops "ERROR" macro conflic with LOG_LEVEL::ERROR
+	// https://stackoverflow.com/questions/27064391/unwanted-header-file-wingdi-h
+	#define NOGDI
+#endif
 #include <ctime>
 #include <fstream>
 #include <memory>
