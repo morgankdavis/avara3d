598ba235546754549ad4f55ac13320f5d0798572
Morgan Davis <mkd@mkd.net>
2024-02-17 12:54:45 -0800

    Fixed pespective camera uninitialized _aspecRatio

--- src/rendering/camera/PerspectiveCamera.cc
+++ src/rendering/camera/PerspectiveCamera.cc
@@ -18,7 +18,8 @@ PerspectiveCamera::PerspectiveCamera():
 		Camera(),
 		_zNear(0.1),
 		_zFar(1000.0),
-		_yFov(radians(45.0)) {}
+		_yFov(radians(45.0)),
+		_aspectRatio(0) {}
 
 PerspectiveCamera::PerspectiveCamera(float zNear, float zFar, float yFov):
 		Camera(),
