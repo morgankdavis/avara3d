818eb57bd890bf3e6e41b5160e0799d0972bd733
Morgan Davis <mkd@mkd.net>
2026-08-04 13:19:40 -0700

    Added periodic trigger and application callback refactor

--- lib/include/a3d/util/PeriodicTrigger.h
+++ lib/include/a3d/util/PeriodicTrigger.h
@@ -1,14 +1,57 @@
 //
-//  App.h
-//  004-geoprimitives
+//  PeriodicTrigger.h
+//  avara3d
 //
-//  Created by Morgan Davis on 7/17/26.
+//  Created by Morgan Davis on 8/4/26.
 //  Copyright © 2026 Morgan K Davis. All rights reserved.
 //
 
-#ifndef AVARA3D_PERIODICTIMER_H
-#define AVARA3D_PERIODICTIMER_H
+#ifndef AVARA3D_UTIL_PERIODICTRIGGER_H
+#define AVARA3D_UTIL_PERIODICTRIGGER_H
 
-class PeriodicTimer {};
+#include <chrono>
+#include <cstddef>
+#include <functional>
+#include <optional>
 
-#endif //AVARA3D_PERIODICTIMER_H
+namespace a3d::util {
+
+    class PeriodicTrigger {
+
+    public:
+        /// Public Lifecycle Functions ///
+
+        explicit PeriodicTrigger(std::chrono::duration<double> interval, bool deferFirstFire = false);
+
+        /// Public Member Functions ///
+
+        template<typename Function>
+        std::size_t update(double time, Function&& function) {
+
+            const auto count = dueCount(time);
+
+            for (std::size_t i = 0; i < count; ++i) {
+                std::invoke(function);
+            }
+
+            return count;
+        }
+
+        void reset() noexcept;
+
+    private:
+        /// Private Member Functions ///
+
+        std::size_t           dueCount(double time);
+
+        /// Private Member Variables ///
+
+        double                _interval;
+        bool                  _deferFirstFire;
+        std::optional<double> _nextFireTime;
+        std::optional<double> _lastTime;
+    };
+
+}
+
+#endif // AVARA3D_UTIL_PERIODICTRIGGER_H
