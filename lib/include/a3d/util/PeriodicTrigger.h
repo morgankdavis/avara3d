a71f08b5434f347be35a512cad4cd57b4da8ed4e
Morgan Davis <mkd@mkd.net>
2026-08-04 13:13:30 -0700

    Added periodic trigger and application callback refactor

---
+++ lib/include/a3d/util/PeriodicTrigger.h
@@ -0,0 +1,14 @@
+//
+//  App.h
+//  004-geoprimitives
+//
+//  Created by Morgan Davis on 7/17/26.
+//  Copyright © 2026 Morgan K Davis. All rights reserved.
+//
+
+#ifndef AVARA3D_PERIODICTIMER_H
+#define AVARA3D_PERIODICTIMER_H
+
+class PeriodicTimer {};
+
+#endif //AVARA3D_PERIODICTIMER_H
