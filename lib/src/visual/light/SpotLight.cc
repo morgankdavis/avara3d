908e7500bf246d170a4fe7878ae93d1e18c8f963
Morgan Davis <4205474+morgankdavis@users.noreply.github.com>
2026-08-20 16:37:31 -0700

    Fix SpotLight default angle initialization

--- lib/src/visual/light/SpotLight.cc
+++ lib/src/visual/light/SpotLight.cc
@@ -20,8 +20,8 @@ using namespace std;
 SpotLight::SpotLight():
     Light() {
     // see DeVries 16.5
-    _innerAngleCos = math::radians(15.0f);
-    _outerAngleCos = math::radians(25.0f);
+    innerAngle(math::radians(15.0f));
+    outerAngle(math::radians(25.0f));
     _featherMode = FeatheringMode::Linear;
 }
 
